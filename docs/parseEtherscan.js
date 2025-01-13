c7efce8ce4db5ca5220954f17bc7e7d992396be7
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-01-13 13:38:02 +1100

    etherscan api wip

---
+++ docs/parseEtherscan.js
@@ -0,0 +1,3 @@
+function parseEtherscanGetTransactionsResult(result) {
+  console.log(moment().format("HH:mm:ss") + " parseEtherscanGetTransactionsResult - result: " + JSON.stringify(result, null, 2).substring(0, 200));
+}
