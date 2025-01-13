848de31a9d37253343ed40cb56c5b09ec634b89b
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-01-14 09:38:24 +1100

    wip

--- docs/parseReservoir.js
+++ docs/parseReservoir.js
@@ -3,8 +3,8 @@ function parseReservoirData(data, address, reservoirData) {
   if (!(address in reservoirData)) {
     reservoirData[address] = {};
   }
-  for (const item of (data && data.tokens.slice(0, 1) || [])) {
-    console.log(moment().format("HH:mm:ss") + " parseReservoirData - item: " + JSON.stringify(item, null, 2));
+  for (const item of (data && data.tokens.slice(4, 5) || [])) {
+    console.log(moment().format("HH:mm:ss") + " parseReservoirData - item: " + JSON.stringify(item, null, 2).substring(0, 20000));
     const chainId = item.token.chainId;
     if (!(('' + chainId) in reservoirData[address])) {
       reservoirData[address][chainId] = {};
@@ -13,7 +13,66 @@ function parseReservoirData(data, address, reservoirData) {
     const type = item.token.kind;
     const tokenId = item.token.tokenId;
     const count = item.ownership.tokenCount;
-    console.log(moment().format("HH:mm:ss") + " parseReservoirData - item: " + chainId + ":" + contract + "(" + type + "):" + tokenId + "x" + count);
+    const collectionName = item.token.collection.name;
+    const collectionSlug = item.token.collection.slug;
+    const name = item.token.name;
+    const description = item.token.description;
+    const image = item.token.image;
+    const acquiredAt = parseInt(Date.parse(item.ownership.acquiredAt)/1000);
+    console.log(moment().format("HH:mm:ss") + " parseReservoirData - item: " + chainId + ":" + contract + "(" + type + "):" + tokenId + "x" + count + " " + new Date(acquiredAt * 1000).toString() + " from " + item.ownership.acquiredAt);
+
+    const token = item.token;
+    const lastSaleTimestamp = token.lastSale && token.lastSale.timestamp || null;
+    const lastSaleCurrency = token.lastSale && token.lastSale.price && token.lastSale.price.currency && token.lastSale.price.currency.symbol || null;
+    const lastSaleAmount = token.lastSale && token.lastSale.price && token.lastSale.price.amount && token.lastSale.price.amount.native || null;
+    const lastSaleAmountUSD = token.lastSale && token.lastSale.price && token.lastSale.price.amount && token.lastSale.price.amount.usd || null;
+    let lastSale = null;
+    if (lastSaleAmount) {
+      lastSale = {
+        timestamp: lastSaleTimestamp,
+        currency: lastSaleCurrency,
+        amount: lastSaleAmount,
+        amountUSD: lastSaleAmountUSD,
+      };
+    }
+    console.log(moment().format("HH:mm:ss") + " parseReservoirData - lastSale: " + JSON.stringify(lastSale, null, 2));
+
+    const priceExpiry = token.floorAsk && token.floorAsk.validUntil && parseInt(token.floorAsk.validUntil) || null;
+    const priceSource = token.floorAsk && token.floorAsk.source && token.floorAsk.source.domain || null;
+    const priceCurrency = token.floorAsk && token.floorAsk.price && token.floorAsk.price.currency && token.floorAsk.price.currency.symbol || null;
+    const priceAmount = token.floorAsk && token.floorAsk.price && token.floorAsk.price.amount && token.floorAsk.price.amount.native || null;
+    const priceAmountUSD = token.floorAsk && token.floorAsk.price && token.floorAsk.price.amount && token.floorAsk.price.amount.usd || null;
+    let price = null;
+    if (priceAmount) {
+      price = {
+        source: priceSource,
+        expiry: priceExpiry,
+        currency: priceCurrency,
+        amount: priceAmount,
+        amountUSD: priceAmountUSD,
+      };
+    }
+    console.log(moment().format("HH:mm:ss") + " parseReservoirData - price: " + JSON.stringify(price, null, 2));
+
+    const topBidCurrency = token.topBid && token.topBid.price && token.topBid.price.currency && token.topBid.price.currency.symbol || null;
+    const topBidSource = token.topBid && token.topBid.source && token.topBid.source.domain || null;
+    const topBidAmount = token.topBid && token.topBid.price && token.topBid.price.amount && token.topBid.price.amount.native || null;
+    const topBidAmountUSD = token.topBid && token.topBid.price && token.topBid.price.amount && token.topBid.price.amount.usd || null;
+    const topBidNetAmount = token.topBid && token.topBid.price && token.topBid.price.netAmount && token.topBid.price.netAmount.native || null;
+    const topBidNetAmountUSD = token.topBid && token.topBid.price && token.topBid.price.netAmount && token.topBid.price.netAmount.usd || null;
+    let topBid = null;
+    if (topBidNetAmount) {
+      topBid = {
+        source: topBidSource,
+        currency: topBidCurrency,
+        amount: topBidAmount,
+        amountUSD: topBidAmountUSD,
+        netAmount: topBidNetAmount,
+        netAmountUSD: topBidNetAmountUSD,
+      };
+    }
+    console.log(moment().format("HH:mm:ss") + " parseReservoirData - topBid: " + JSON.stringify(topBid, null, 2));
+
   }
   console.log(moment().format("HH:mm:ss") + " parseReservoirData - reservoirData: " + JSON.stringify(reservoirData, null, 2));
 
