0df443bbceba54c16bb21165ef07828e10fe349b
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-01-13 15:14:03 +1100

    etherscan api wip

--- docs/parseEtherscan.js
+++ docs/parseEtherscan.js
@@ -1,3 +1,39 @@
-function parseEtherscanGetTransactionsResult(result) {
+function parseEtherscanGetTransactionsResult(result, chainId, etherscanData) {
   console.log(moment().format("HH:mm:ss") + " parseEtherscanGetTransactionsResult - result: " + JSON.stringify(result, null, 2).substring(0, 200));
+  const results = [];
+  if (result.status == 1 && result.message == "OK" && result.result) {
+    if (!(('' + chainId) in etherscanData)) {
+      etherscanData[chainId] = {};
+    }
+    for (const tx of result.result) {
+      console.log(moment().format("HH:mm:ss") + " parseEtherscanGetTransactionsResult - tx: " + JSON.stringify(tx, null, 2).substring(0, 200));
+      if (!(tx.hash in etherscanData[chainId])) {
+        etherscanData[chainId][tx.hash] = {
+          tx: {
+            blockNumber: tx.blockNumber,
+            timeStamp: tx.timeStamp,
+            hash: tx.hash,
+            nonce: tx.nonce,
+            blockHash: tx.blockHash,
+            transactionIndex: tx.transactionIndex,
+            from: ethers.utils.getAddress(tx.from),
+            to: tx.to && ethers.utils.getAddress(tx.to) || null,
+            value: tx.value,
+            gas: tx.gas,
+            gasPrice: tx.gasPrice,
+            isError: tx.isError,
+            txreceipt_status: tx.txreceipt_status,
+            input: tx.input,
+            contractAddress: tx.contractAddress && ethers.utils.getAddress(tx.contractAddress) || null,
+            cumulativeGasUsed: tx.cumulativeGasUsed,
+            gasUsed: tx.gasUsed,
+            confirmations: tx.confirmations,
+            methodId: tx.methodId,
+            functionName: tx.functionName,
+          },
+        };
+      }
+    }
+  }
+
 }
