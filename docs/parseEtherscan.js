fa402c9812b27ee58f5017d2c44ea7ea1b4c33ad
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2025-01-14 15:19:35 +1100

    etherscan data

--- docs/parseEtherscan.js
+++ docs/parseEtherscan.js
@@ -2,7 +2,6 @@ function parseEtherscanGetTransactionsResult(result, chainId, etherscanData) {
   console.log(moment().format("HH:mm:ss") + " parseEtherscanGetTransactionsResult - result: " + JSON.stringify(result, null, 2).substring(0, 200));
   const results = [];
   if (result.status == 1 && result.message == "OK" && result.result) {
-    // TODO: Add address
     if (!(('' + chainId) in etherscanData)) {
       etherscanData[chainId] = {};
     }
@@ -32,9 +31,47 @@ function parseEtherscanGetTransactionsResult(result, chainId, etherscanData) {
             methodId: tx.methodId,
             functionName: tx.functionName,
           },
+          internal: null,
+          erc20: null,
+          erc721: null,
+          erc1155: null,
         };
       }
     }
   }
+}
 
+
+function parseEtherscanGetInternalTransactionsResult(result, chainId, etherscanData) {
+  console.log(moment().format("HH:mm:ss") + " parseEtherscanGetInternalTransactionsResult - result: " + JSON.stringify(result, null, 2).substring(0, 2000));
+  const results = [];
+  if (result.status == 1 && result.message == "OK" && result.result) {
+    if (!(('' + chainId) in etherscanData)) {
+      etherscanData[chainId] = {};
+    }
+    for (const tx of result.result) {
+      console.log(moment().format("HH:mm:ss") + " parseEtherscanGetInternalTransactionsResult - tx: " + JSON.stringify(tx, null, 2).substring(0, 2000));
+      if (!(tx.hash in etherscanData[chainId])) {
+        etherscanData[chainId][tx.hash] = {
+          tx: null,
+          internal: {
+            blockNumber: tx.blockNumber,
+            timeStamp: tx.timeStamp,
+            hash: tx.hash,
+            from: ethers.utils.getAddress(tx.from),
+            to: tx.to && ethers.utils.getAddress(tx.to) || null,
+            value: tx.value,
+            contractAddress: tx.contractAddress,
+            input: tx.input,
+            type: tx.type,
+            gas: tx.gas,
+            gasUsed: tx.gasUsed,
+          },
+          erc20: null,
+          erc721: null,
+          erc1155: null,
+        };
+      }
+    }
+  }
 }
